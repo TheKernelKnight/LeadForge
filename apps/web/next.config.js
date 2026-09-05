8bb1eb4722f4224717e991845366c608ea71d3f4
TheKernelKnight <thekernelknight@gmail.com>
2026-09-05 22:02:42 +0100

    .

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
+  distDir: '.next',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   output: 'standalone',
-};
+  images: {
+    unoptimized: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
